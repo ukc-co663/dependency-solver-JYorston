f9d2eaa0692144522bb73874c72662a07f143caa
James Yorston <Jamesyorston1994@gmail.com>
2018-02-28 14:33:11 +0000

    remove comments for actual tests

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -13,25 +13,25 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-////
-        String currentTest = "seen-9";
-
-        String basePath = Paths.get(".").toAbsolutePath().normalize().toString();
-        String repoPath = basePath + "/tests/" + currentTest +"/repository.json";
-        String initPath = basePath + "/tests/" + currentTest +"/initial.json";
-        String constPath = basePath + "/tests/" + currentTest +"/constraints.json";
+//////
+//        String currentTest = "seen-9";
 //
-        TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
-        List<Package> repo = JSON.parseObject(readFile(repoPath), repoType);
-        TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
-        List<String> initial = JSON.parseObject(readFile(initPath), strListType);
-        List<String> constraints = JSON.parseObject(readFile(constPath), strListType);
-
+//        String basePath = Paths.get(".").toAbsolutePath().normalize().toString();
+//        String repoPath = basePath + "/tests/" + currentTest +"/repository.json";
+//        String initPath = basePath + "/tests/" + currentTest +"/initial.json";
+//        String constPath = basePath + "/tests/" + currentTest +"/constraints.json";
+////
 //        TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
-//        List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
+//        List<Package> repo = JSON.parseObject(readFile(repoPath), repoType);
 //        TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
-//        List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
-//        List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
+//        List<String> initial = JSON.parseObject(readFile(initPath), strListType);
+//        List<String> constraints = JSON.parseObject(readFile(constPath), strListType);
+
+        TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
+        List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
+        TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
+        List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
+        List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
 
         HashMap<String,Package> packageMap = buildPackageMap(repo);
         HashSet<Package> initialSet = parseInitial(initial,packageMap);
@@ -119,7 +119,6 @@ public class Main {
 
         // Does the current state satisfy required packages
         if(stateContainsRequiredPackages(state,cs)){
-//        if(state.containsAll(cs.getRequiredPackages())){
             if(Collections.disjoint(state,cs.getRequiredMissingPackages())){
                 List<List<String>> finalCommands = new ArrayList<>();
                 finalCommands.add(commands);
